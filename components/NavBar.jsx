fd051f6c2a4226eed70abffd446d9e0541a8b808
Esteban Barac <EstebanBarac8@gmail.com>
2022-11-29 21:22:20 -0300

    foto 7

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -5,7 +5,7 @@ import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 const Navbar = () => {
 
     const [nav, setNav] = useState(false);
-    const [color, setColor] = useState('transparent');
+    const [color, setColor] = useState('black');
     const [textcolor, setTextColor] = useState('white');
 
     const handleNav = () => {
@@ -32,7 +32,7 @@ const Navbar = () => {
 
 
   return (
-    <div style={{backgroundColor: `${color}`}} className='fixed left-0 top-0 w-full z-10 ease-in duration-300'>
+    <div style={{backgroundColor: `${color}`}} className='fixed left-0 top-0 w-full bg-stone-900 z-10 ease-in duration-300'>
         <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>
             <Link href='/'>
             <h1 style={{color: `${textcolor}`}} className='font-bold text-4xl'>ESBA DIVE</h1>
